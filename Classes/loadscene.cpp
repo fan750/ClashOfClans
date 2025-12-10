668571f0393cabfd9ccb674875d6ac5398c32c4b
wwwchangjin <changjinwang1226@tongji.edu.cn>
2025-12-10 20:42:23 +0800

    添加了地图层，现在地图可以拖动。美化了部分ui

--- Classes/loadscene.cpp
+++ Classes/loadscene.cpp
@@ -1,27 +1,3 @@
-/****************************************************************************
- Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
- http://www.cocos2d-x.org
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- ****************************************************************************/
-
 #include "loadscene.h"
 #include "SimpleAudioEngine.h"
 #include"HelloWorldScene.h"
@@ -45,7 +21,7 @@ bool Load::init()
 {
     //////////////////////////////
     // 1. super init first
-    if ( !Scene::init() )
+    if (!Scene::init())
     {
         return false;
     }
@@ -65,7 +41,7 @@ bool Load::init()
         Size texSize = sprite->getContentSize();
         float scaleX = visibleSize.width / texSize.width;
         float scaleY = visibleSize.height / texSize.height;
-        float scale = std::max(scaleX, scaleY); // 等比缩放，完整显示
+        float scale = std::max(scaleX, scaleY); // ??±???·????ê??????
         sprite->setScale(scale);
         sprite->setPosition(visibleSize / 2);
         layer->addChild(sprite);
